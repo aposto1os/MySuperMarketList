ac4aa7dc689dceaaf5656fb8ece6c48e92adbea6
Ap Panta <ap.pantazopoulos@yahoo.com>
2024-06-30 08:39:57 +0300

    Refactor testDeodorant and add test for shampoo in myMarketTests.

--- src/test/java/myMarketTests/TestMyMarket.java
+++ src/test/java/myMarketTests/TestMyMarket.java
@@ -6,18 +6,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestMyMarket {
-    private WebDriver driver = new ChromeDriver();
+public class TestMyMarket{
 
+    private WebDriver driver;
+
+    public TestMyMarket() {
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void testDeodorant(){
+        String returnedProductName = getProductName("Dove advanced care coconut spray");
+        Assert.assertEquals(returnedProductName, "Dove Αποσμητικό Σωμ. Spray Advanced Care Coconut 150ml", "Incorrect product returned!");
+    }
+
+    @Test
+    private void testShampoo(){
+        String returnedProductName = getProductName("wash & go Σαμπουάν classic 650ml");
+        Assert.assertEquals(returnedProductName, "Wash & Go 2in1 Σαμπουάν Classic 650 ml", "Incorrect product returned!");
+    }
+
+    private String getProductName(String product){
         MyMarket myMarket = new MyMarket(driver);
         myMarket.getHomePage();
         String returnedProductName = myMarket.
-                searchFor("Dove advanced care coconut spray").
+                searchFor(product).
                 getFirstResultProduct().
                 getProductName();
-        Assert.assertEquals(returnedProductName, "Dove Αποσμητικό Σωμ. Spray Advanced Care Coconut 150ml", "Incorrect product returned!");
+        return returnedProductName;
     }
 }
